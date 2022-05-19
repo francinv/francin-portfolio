fa58a67acafbadc65c281d0f87768fa27ef29fa6
Francin Anoj Vincent <francinv@stud.ntnu.no>
2022-05-19 10:44:59 +0200

    :lipstick: (#16) Rewrite animation
    Removed style and used states to determine if aboutMe should be shown.

--- portfolio/components/Terminal/TerminalContent.tsx
+++ portfolio/components/Terminal/TerminalContent.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useContext } from "react";
+import React, { FC, useContext, useState } from "react";
 import style from "./Terminal.module.css";
 import marginStyles from "../../styles/margin.module.css";
 import colorStyles from "../../styles/colors.module.css";
@@ -7,8 +7,46 @@ import layoutStyles from "../../styles/layout.module.css";
 import { PortfolioContext } from "../../features/AppContext";
 
 const TerminalContent: FC = () => {
+    const [isVisible, setIsVisible] = useState(false);
     const { fullSizeModal } = useContext(PortfolioContext);
     const command = "(base) francinvincent@Francins-MBP ~ % cat aboutFrancin.json";
+
+    interface AboutProps {
+        isVisible: boolean;
+    }
+
+    const About: FC<AboutProps> = ({isVisible}) => {
+        if (isVisible) {
+            return (
+                <p 
+                    className={`
+                        ${colorStyles.text_white} 
+                        ${marginStyles.mt_5} 
+                        ${commonStyles.text_16} 
+                        ${commonStyles.text_monaco}`}>
+                    {"{"}
+                    <br />
+                        <span 
+                            className={`
+                                ${marginStyles.mx_5} 
+                                ${marginStyles.ml_16} 
+                                ${commonStyles.text_16} 
+                                ${commonStyles.text_monaco}
+                                ${layoutStyles.flex_col}`}
+                            >
+                                <span className={marginStyles.mx_2}>{"“name”: “Francin Vincent,”"}</span>
+                                <span className={marginStyles.mx_2}>{"“age”: “23,”"}</span>
+                                <span className={marginStyles.mx_2}>{"“education”: “Bachelor in Informatics (completing this summer)”,"}</span>
+                                <span className={marginStyles.mx_2}>{"“languages”: “Java, Python, C, C#, JavaScript, TypeScript”,"}</span>
+                                <span className={marginStyles.mx_2}>{"“frameworks”: “Django, React, React Native, ASP.NET, Redux”"}</span>
+                        </span>
+                    {"}"}
+                </p>
+            )
+        } else {
+            return null;
+        }
+    }
 
     return (
         <div className={`${style.terminal_content_container} ${marginStyles.my_auto} ${marginStyles.mt_12}`}>
@@ -20,38 +58,9 @@ const TerminalContent: FC = () => {
                     ${commonStyles.text_monaco} 
                     ${fullSizeModal ? "full_screen_animation" : "minor_screen_animation" }
                 `}
-                onAnimationEnd={() => {
-                    const element = document.querySelector(".display_none");
-                    element?.classList.remove("display_none");
-                    element?.classList.add("display_block");
-                }}
-                    
-            >{command}</p>
-            <p 
-                className={`
-                    ${colorStyles.text_white} 
-                    ${marginStyles.mt_5} 
-                    ${commonStyles.text_16} 
-                    ${commonStyles.text_monaco}
-                    display_none`}>
-                {"{"}
-                <br />
-                    <span 
-                        className={`
-                            ${marginStyles.mx_5} 
-                            ${marginStyles.ml_16} 
-                            ${commonStyles.text_16} 
-                            ${commonStyles.text_monaco}
-                            ${layoutStyles.flex_col}`}
-                        >
-                            <span className={marginStyles.mx_2}>{"“name”: “Francin Vincent,”"}</span>
-                            <span className={marginStyles.mx_2}>{"“age”: “23,”"}</span>
-                            <span className={marginStyles.mx_2}>{"“education”: “Bachelor in Informatics (completing this summer)”,"}</span>
-                            <span className={marginStyles.mx_2}>{"“languages”: “Java, Python, C, C#, JavaScript, TypeScript”,"}</span>
-                            <span className={marginStyles.mx_2}>{"“frameworks”: “Django, React, React Native, ASP.NET, Redux”"}</span>
-                    </span>
-                {"}"}
-            </p>
+                onAnimationStart={() => setIsVisible(false)}
+                onAnimationEnd={() => setIsVisible(true)}>{command}</p>
+            <About isVisible={isVisible} />
         </div>
     );
 }
